8c64815d92d2b803fed4756b2ba3ac5df6bfc9d2
Graciliano Martins Fontes Neto <gracilianoneto@icloud.com>
2020-12-27 01:31:58 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBslhbGbjZ-sql1u0UrRw0ZARafs-lmzTA",
-    authDomain: "camp-game-app.firebaseapp.com",
-    databaseURL: "https://camp-game-app.firebaseio.com",
-    projectId: "camp-game-app",
-    storageBucket: "camp-game-app.appspot.com",
-    messagingSenderId: "56563135306",
-    appId: "1:56563135306:web:b90a0d41eaaf9e486e47b3"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   }
 };
 
